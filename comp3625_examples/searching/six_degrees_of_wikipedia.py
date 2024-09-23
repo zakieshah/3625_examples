c24ef6424208c8fe84bc58de1b6348aa746d824c
echalmers <dchalmer@ualberta.ca>
2024-09-23 09:21:15 -0600

    Update six_degrees_of_wikipedia.py

--- comp3625_examples/searching/six_degrees_of_wikipedia.py
+++ comp3625_examples/searching/six_degrees_of_wikipedia.py
@@ -9,3 +9,17 @@ start = WikiPage('https://en.wikipedia.org/wiki/Breadth-first_search')
 goal = WikiPage('https://en.wikipedia.org/wiki/New_York_City')
 
 # write a search algorithm to find a path from start to goal
+frontier = Queue()
+frontier.put(start)
+reached = {start}
+
+while not frontier.empty():
+    state = frontier.get()
+    for child in state.children:
+        
+        if child == goal:
+            print(child.get_ancestors())
+            exit()
+        if child not in reached:
+            reached.add(child)
+            frontier.put(child)
